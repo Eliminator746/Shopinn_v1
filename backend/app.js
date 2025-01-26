2030616b8e0a5d17b3e5da9ef1cf2f90df49eca6
Eliminator746 <90308790+Eliminator746@users.noreply.github.com>
2025-01-26 17:32:42 +0530

    Added Express, MongoDB connection, and utilities

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+// ------------------------------------------------------------------------------------------------------------------------
+//                                                          Middleware
+// ------------------------------------------------------------------------------------------------------------------------
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,   
+    credentials : true
+}));
+
+// Handles the incoming request with JSON payload
+app.use(express.json({limit : "16kb"}));
+
+
+// Handles the incoming request with URL encoded payload
+app.use(express.urlencoded({extended : true, limit : "16kb"}));
+// extended : true allows to parse the nested object
+
+// Serve static files
+app.use(express.static("public"))
+// public is the folder where we store the static files like images, css, pdf, js etc.
+
+
+// Handles the incoming request with cookies
+app.use(cookieParser());
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+export default app; 
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import connectDB from './src/db/index.js';
+import app from './app.js';
+
+dotenv.config({
+  path: '../.env',
+});
+
+connectDB()
+  .then(() => {
+    app.on('error', (err) => {
+      console.error('Error: ' + err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error: ' + err);
+    throw err;
+  });
