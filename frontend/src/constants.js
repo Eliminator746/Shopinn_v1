ce036ddfe065a790d6c824c123ebb59a72339407
Eliminator746 <90308790+Eliminator746@users.noreply.github.com>
2025-01-30 14:20:54 +0530

    Added redux boilerplate

---
+++ frontend/src/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = process.env.NODE_ENV === 'develeopment' ? 'http://localhost:8000' : '';
+// export const BASE_URL = ''; // If using proxy
---
+++ frontend/src/features/apiSlice.js
@@ -0,0 +1,14 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from '../constants.js';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+});
+
+// Define a service using a base URL and expected endpoints
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ['Product', 'Order', 'User'], // used to define types of data we are fetching from APIs
+  endpoints: (builder) => ({}),
+});
