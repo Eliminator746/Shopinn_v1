fd850e1aef72c4d93d393df594f8177138008271
Eliminator746 <90308790+Eliminator746@users.noreply.github.com>
2025-02-04 15:39:39 +0530

    Added authentication slice and user API endpoints for login, registration, and logout functionality

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../features/apiSlice.js';
 import cartSliceReducer from '../features/cartSlice.js'
+import authReducer from '../features/authSlice.js'
 
 export const store = configureStore({
 
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
+    auth: authReducer,
   }, 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
